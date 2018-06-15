1d73ee2200a70081d5ae1aadf8926ac25acb4a9a
Arkady Fukzon <arkady.fukzon@gmail.com>
2018-06-15 18:35:49 +0300

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,11 @@ git push origin dev
 
 cp -R dist dist_temp
 git checkout master
-rm -rf dist
-mv dist_temp dist
+shopt -s extglob
+rm -rf !(node_modules|.gitignore|dist_temp)
+cp -R dist_temp/ .
+rm -rf dist_temp
 git add .
 git commit -am "update master"
 git push origin master
+git checkout dev
