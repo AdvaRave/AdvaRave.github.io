1a7ea9a7e8141417cefe1f264d13221fbf4fe5e9
Arkady Fukzon <arkady.fukzon@gmail.com>
2018-06-15 17:26:37 +0300

    add meta tags

--- src/pages/creojam.vue
+++ src/pages/creojam.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        Under Construction
-    </div>
+    <article>
+        <h1 class="img"></h1>
+        <a class="button">Take me to Creojam</a>
+        <section>
+            <h2>The Problem</h2>
+            <p>
+            </p>
+        </section>
+    </article>
 </template>
 
 <script>
